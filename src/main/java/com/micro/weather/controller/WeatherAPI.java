1e4a6ac50968f9612bc2e7b84628c8c4bab28c3e
İhsan Algül <algulihsan@hotmail.com>
2023-04-07 00:55:14 +0200

    add constraint and validation

--- src/main/java/com/micro/weather/controller/WeatherAPI.java
+++ src/main/java/com/micro/weather/controller/WeatherAPI.java
@@ -2,7 +2,10 @@ package com.micro.weather.controller;
 
 import com.micro.weather.dto.WeatherDTO;
 import com.micro.weather.service.WeatherService;
+import com.micro.weather.validation.CityNameConstraint;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/api/weather")
+@Validated
 public class WeatherAPI {
 
     private final WeatherService weatherService;
@@ -19,7 +23,7 @@ public class WeatherAPI {
     }
 
     @GetMapping("/{city}")
-    public ResponseEntity<WeatherDTO> getWeather(@PathVariable("city") String city) {
+    public ResponseEntity<WeatherDTO> getWeather(@PathVariable("city") @CityNameConstraint @NotBlank String city) {
         return ResponseEntity.ok(weatherService.getWeatherByCityName(city));
     }
 
---
+++ src/main/java/com/micro/weather/validation/CityNameConstraint.java
@@ -0,0 +1,23 @@
+package com.micro.weather.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = {CityNameValidator.class})
+@Target({METHOD, FIELD, PARAMETER})
+@Retention(RUNTIME)
+public @interface CityNameConstraint {
+    String message() default "Invalid city name.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+
+}
